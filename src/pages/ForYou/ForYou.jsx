05b1aca17c0b7f4af69056f29ff14d767b97e4eb
Hyeyun <yuuuuuuni@gmail.com>
2022-03-09 23:12:24 +0900

    init pages

---
+++ src/pages/ForYou/ForYou.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ForYou = () => {
+  return (
+    <div>
+      For You
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default ForYou;
