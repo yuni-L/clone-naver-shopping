feed456853cc3e21f4efd7306ff55e3f504fe0a5
Hyeyun <yuuuuuuni@gmail.com>
2022-03-09 23:12:05 +0900

    init router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import Gnb from './layout/gnb/gnb'
+import Gnb from './layout/gnb/gnb';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home'
+import ForYou from './pages/ForYou/ForYou'
+
 
 function App() {
   return (
-    <div className="App">
-      <Gnb />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/foryou" element={<ForYou />} />
+    </Routes>
+    // <div className="App">
+    //   <Gnb />
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
