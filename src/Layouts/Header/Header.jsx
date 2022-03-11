aeb521d5e9148629c55d1b46beca4fad94b0be9e
Hyeyun <yuuuuuuni@gmail.com>
2022-03-11 21:36:58 +0900

    header main mark-up

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import './Header.scss';
+
 const Header = () => {
   return (
     <div>
-      <header>헤더가 될 부분</header>
+      <header className='header'>
+      {/* header-main start */}
+        <div className='header-main'>
+          <div className='header-main-menu'>
+            <span className='logo'>네이버 쇼핑 따라하기</span>
+            <span className='material-icons-round'>menu</span>
+          </div>
+          <div className='header-main-input'>
+            <input placeholder='상품검색'></input>
+            <span className='material-icons-outlined'>camera_alt</span>
+          </div>
+        </div>
+        {/* header-main end */}
+        <div className='header-sub'></div>
+      </header>
       <main>
         <Outlet />
       </main>
