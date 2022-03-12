dcce44180a804de1a9c7291232fe540709e0445b
Hyeyun <yuuuuuuni@gmail.com>
2022-03-12 16:45:28 +0900

    header sub mark-up

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 import './Header.scss';
+
+const SUB_MENU = [
+  {name: '홈', href: ''},
+  {name: 'FOR YOU', href: 'foryou'},
+  {name: '원플딜', href: 'foryou'},
+  {name: '벡화점윈도', href: 'foryou'},
+  {name: '아울렛윈도', href: 'foryou'},
+  {name: '스타일윈도', href: 'foryou'},
+  {name: '디자이너윈도', href: 'foryou'},
+  {name: '뷰티윈도', href: 'foryou'},
+  {name: '럭셔리', href: 'foryou'},
+  {name: '리빙윈도', href: 'foryou'},
+  {name: '푸드윈도', href: 'foryou'},
+  {name: '장보기', href: 'foryou'},
+  {name: '키즈윈도', href: 'foryou'},
+  {name: '펫윈도', href: 'foryou'},
+  {name: '플레이윈도', href: 'foryou'},
+  {name: '아트윈도', href: 'foryou'},
+  {name: '핫딜', href: 'foryou'},
+  {name: '핫딜2', href: 'foryou'},
+  {name: '핫딜3', href: 'foryou'},
+  {name: '핫딜4', href: 'foryou'},
+  {name: '핫딜5', href: 'foryou'},
+  {name: '핫딜6', href: 'foryou'},
+  {name: '핫딜7', href: 'foryou'},
+]
 
 const Header = () => {
   return (
@@ -19,7 +45,19 @@ const Header = () => {
           </div>
         </div>
         {/* header-main end */}
-        <div className='header-sub'></div>
+        <div className='header-sub'>
+          <div className='header-sub-scroll'>
+            <ul>
+              {SUB_MENU.map((menu, index) => (
+                  <li key={index}>
+                    <Link to={'/' + menu.href}>{menu.name}</Link>
+                  </li>
+              ))}
+            </ul>
+          </div>
+          <span className='header-sub-arrow material-icons'>keyboard_arrow_down</span>
+          {/* <span className='material-icons'>keyboard_arrow_up</span> */}
+        </div>
       </header>
       <main>
         <Outlet />
